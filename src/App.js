ba71a4c468de49bbf96409a5932d4f2af0e3d430
Rahadian Reza Rizaldy <59022153+apaajaa22@users.noreply.github.com>
2021-04-27 23:35:11 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App">haioo</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
